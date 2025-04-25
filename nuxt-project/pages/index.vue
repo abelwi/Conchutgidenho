1a0b6b1e4e3e7cac2535730de9eb84f6b7188793
Lam <huyenhuong3010@gmail.com>
2025-04-25 03:56:34 +0000

    install tailwind css

---
+++ nuxt-project/pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1 class="mt-10 bg-green-300 p-2 rounded-lg mx-10">Hi welcome to my world</h1>
+    <button>Click me</button>
+</template>
---
+++ nuxt-project/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      './pages/**/*.{vue,js}',
+      './components/**/*.{vue,js}',
+      './layouts/**/*.{vue,js}',
+      './composables/**/*.{vue,js}',
+      './nuxt.config.ts', // Don't forget to include nuxt.config.ts for full build-time purging
+    ],
+    theme: {
+      extend: {},
+    },
+  }
