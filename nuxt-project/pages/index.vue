d0e530c5a2393e9a4327fc225f17801f7f570958
Lam <huyenhuong3010@gmail.com>
2025-04-28 09:15:36 +0000

    style heading page

--- nuxt-project/pages/index.vue
+++ nuxt-project/pages/index.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
-      <img src="/images/my-avatar.jpg" alt="My avatar" />
+    <div class="bg-[url(/images/my-avatar.jpg)] bg-cover bg-no-repeat bg-[20%_30%] pb-24">
+      <div class="w-1/3 space-y-7">
+        <h1 class="font-serif flex justify-center text-white text-4xl pt-20"><strong>Còn chút gì để nhớ ?!</strong></h1>
+        <p class="flex justify-center font-mono italic">Cuộc sống trong đôi mắt này <br>
+          Có vui có buồn có cả mây bay
+        </p>
+        <button class="btn ml-40">Tìm hiểu thêm</button>
+      </div>
     </div>
   </template>
   
