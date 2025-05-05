dd475b5b8956c973632801b45e32c4950eb10327
Lam <huyenhuong3010@gmail.com>
2025-05-05 04:30:08 +0000

    create foreword

---
+++ nuxt-project/components/ForeWord.vue
@@ -0,0 +1,18 @@
+<template>
+    <div v-if="show" class="fixed inset-0 bg-[#F6CF81] z-50 flex flex-col items-center justify-center p-10">
+      <button @click="$emit('close')" class="absolute top-4 right-6 text-2xl">✖</button>
+      <h2 class="text-4xl font-serif mb-6">Câu chuyện của tôi</h2>
+      <p class="text-lg font-mono italic">
+        Đây là một phần nội dung bạn có thể thêm cho câu chuyện.<br />
+        Có thể kéo dài nhiều đoạn, hình ảnh hoặc hoạt cảnh ở đây.
+      </p>
+    </div>
+  </template>
+  
+  <script setup>
+  defineProps({
+    show: Boolean
+  })
+  defineEmits(['close'])
+  </script>
+  
--- nuxt-project/pages/index.vue
+++ nuxt-project/pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="h-screen">
-    <div class="bg-[url(/images/my-avatar.jpg)] bg-cover bg-no-repeat bg-[20%_30%] h-1/2">
+    <div class="bg-[url(/images/my-avatar.jpg)] bg-cover bg-no-repeat bg-[20%_30%] h-1/2 cursor-pointer"
+    @click="showStory = true"
+    >
       <div class="w-2/5 h-full inline-flex flex-col items-center justify-center space-y-7">
         <h1 class="font-serif text-5xl"><strong>Còn chút gì để nhớ?</strong></h1>
         <p class="flex justify-center font-mono italic">Cuộc sống trong đôi mắt này <br>
@@ -19,5 +21,15 @@
       <p class=" w-1/4 h-full pl-16 flex items-center justify-center font-serif text-3xl tracking-wide">Còn chút gì để nhớ!?</p>
     </div>
   </div>
+
+  <ForeWord :show="showStory" @close="showStory = false" />
   </template>
+
+<script setup>
+import { ref } from 'vue'
+import ForeWord from '~/components/ForeWord.vue'
+
+const showStory = ref(false)
+</script>
+
   
